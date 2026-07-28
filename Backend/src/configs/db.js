1c6b28d42d119737ed8daf9bd6bddca216c8dfe9
ffabhishek116@gmail.com <ffabhishek116@gmail.com>
2026-07-29 01:04:12 +0530

    fix: update the envirnment variable

--- Backend/src/configs/db.js
+++ Backend/src/configs/db.js
@@ -4,7 +4,7 @@ const { default: mongoose } = require('mongoose');
 const mongoDB = async () => {
 
     try {
-        await mongoose.connect('mongodb+srv://ffabhishek116_db_user:Abhishekraj@cluster0.zxjqoof.mongodb.net/?appName=Cluster0');
+        await mongoose.connect(process.env.DB_URL);
         console.log("MongoDB Connected successfully...")
     } catch (err) {
         console.log(err.message)
